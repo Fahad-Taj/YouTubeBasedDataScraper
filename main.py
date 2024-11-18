498963ed711dc46d70d95d33e153226b65d0e752
Fahad Taj <fahadtaj909@gmail.com>
2024-11-18 21:10:37 +0530

    New method is working now

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from FileReader import FileReader
 from transcript_download import fetch_transcript
 import os
+from github_transcript_downloader import *
 
 def create_directory(directory_name):
     """Create a subdirectory if it does not exist."""
@@ -17,4 +18,23 @@ links = file_reader.read_file() # 'links' is a list which contains all the links
 
 for link in links:
     print("link: ", link)
-    fetch_transcript(link, "Data Structures and Algorithms in Python - Greg Hogg")
+    video_id = get_video_id(link)
+
+    if video_id:
+        transcript_text = download_transcript(video_id)
+        if transcript_text:
+            video_title = get_video_title(video_id)
+            print(video_title)
+            file_name = f"{video_title}.txt"
+            file_name = re.sub(r'[\\/*?:"<>|]', '', file_name)  # Remove invalid characters
+
+            file_path = os.path.join(output_dir, file_name)
+
+            with open(file_path, 'w', encoding='utf-8') as file:
+                file.write(transcript_text)
+
+            print(f"Transcript saved to {file_name}")
+        else:
+            print("Unable to download transcript.")
+    else:
+        print("Invalid YouTube URL.")
